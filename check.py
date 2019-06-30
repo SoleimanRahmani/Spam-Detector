f2d97c838c6feeb0e56a5d98c77062c76186fd4a
SoleimanRahmani <45254300+SoleimanRahmani@users.noreply.github.com>
2019-06-29 20:42:57 -0400

    Update check.py

--- check.py
+++ check.py
@@ -1,14 +1,18 @@
-import xgboost as xgboost
 import pandas as pd
-from sklearn.decomposition import PCA
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.tree import tree
 from mleap.sklearn import svm
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score, confusion_matrix
+import xgboost as xgboost
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
 
 
 
@@ -19,19 +23,18 @@ df['label'] = df['class'].map({'ham': 0, 'spam': 1})
 X = df['message']
 y = df['label']
 
+print(df.head(10))
+
 # Extract Feature With CountVectorizer
 cv = CountVectorizer()
 X = cv.fit_transform(X)  # Fit the Data
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Naive Bayes Classifier
+print([np.shape(X_train),np.shape(y_train), np.shape(X_test),np.shape(y_test)])
 
-clf = MultinomialNB()
-clf.fit(X_train, y_train)
-pred = clf.predict(X_test)
-prediction_score = round(clf.score(X_test, y_test) * 100, 2)
 
+# Classifiers
 classifiers = []
 model1 = xgboost.XGBClassifier(base_score=0.5, booster='gbtree', colsample_bylevel=1,
             colsample_bynode=1, colsample_bytree=1, gamma=0, learning_rate=0.1,
@@ -48,7 +51,7 @@ model2 = svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
             shrinking=True, tol=0.001, verbose=False)
 classifiers.append(model2)
 
-model3 = tree.DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,
+model3 = DecisionTreeClassifier(class_weight=None, criterion='gini', max_depth=None,
             max_features=None, max_leaf_nodes=None,
             min_impurity_decrease=0.0, min_impurity_split=None,
             min_samples_leaf=1, min_samples_split=2,
@@ -73,9 +76,50 @@ for clf in classifiers:
     y_pred = clf.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     print("Accuracy of %s is %s" % (clf, acc))
-    print("--------------------------------------------------------------------------")
     cm = confusion_matrix(y_test, y_pred)
     print("Confusion Matrix of %s is %s" % (clf, cm))
-    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
 
 
+
+# Tune Naive Bayes Classifier
+
+list_alpha = np.arange(1/100000, 20, 0.11)
+score_train = np.zeros(len(list_alpha))
+score_test = np.zeros(len(list_alpha))
+recall_test = np.zeros(len(list_alpha))
+precision_test= np.zeros(len(list_alpha))
+count = 0
+for alpha in list_alpha:
+    bayes = MultinomialNB(alpha=alpha)
+    bayes.fit(X_train, y_train)
+    score_train[count] = bayes.score(X_train, y_train)
+    score_test[count]= bayes.score(X_test, y_test)
+    recall_test[count] = metrics.recall_score(y_test, bayes.predict(X_test))
+    precision_test[count] = metrics.precision_score(y_test, bayes.predict(X_test))
+    count = count + 1
+matrix = np.matrix(np.c_[list_alpha, score_train, score_test, recall_test, precision_test])
+models = pd.DataFrame(data = matrix, columns =
+             ['alpha', 'Train Accuracy', 'Test Accuracy', 'Test Recall', 'Test Precision'])
+print(models.head(n=10))
+
+print('The model with best test Accuracy')
+best_index = models['Test Accuracy'].idxmax()
+print(models.iloc[best_index, :])
+
+print('Some top models with best test Accuracy')
+print(models[models['Test Accuracy']>=0.97].head(n=5))
+
+
+best_index = models[models['Test Precision']>=0.9]['Test Accuracy'].idxmax()
+bayes = MultinomialNB(alpha=list_alpha[best_index])
+bayes.fit(X_train, y_train)
+print(models.iloc[best_index, :])
+
+
+m_confusion_test = metrics.confusion_matrix(y_test, bayes.predict(X_test))
+print(pd.DataFrame(data = m_confusion_test, columns = ['Predicted 0', 'Predicted 1'],
+            index = ['Actual 0', 'Actual 1']))
+
+
+from sklearn.externals import joblib
+joblib.dump(bayes, 'NB_spam_model.pkl')
